b61064e050efd8585ba2ec88e55dfd58b9c375cd
kbarne12 <kbarne12@gmail.com>
2021-03-29 16:07:17 -0400

    create

--- controllers/classes.js
+++ controllers/classes.js
@@ -4,9 +4,18 @@ const Class = require('../models/class')
 module.exports = {
     index,
     new: newClass,
+    create
     
 }
-
+function create(req, res) {
+    const classs = new Class(req.body);
+    classs.save(function(err) {
+        if(err) return res.render('classes/new');
+        res.redirect(`/classes/${classs._id}`,{title: '',
+        user: req.user
+    })
+    })
+}
 function newClass(req, res) {
     res.render('classes/new', {title: 'Add Classes',
     user: req.user
--- routes/classes.js
+++ routes/classes.js
@@ -3,6 +3,7 @@ const classCtrl = require('../controllers/classes')
 
 router.get("/", isLoggedIn, classCtrl.index);
 router.get('/new', isLoggedIn, classCtrl.new);
+router.post('/', isLoggedIn, classCtrl.create);
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
